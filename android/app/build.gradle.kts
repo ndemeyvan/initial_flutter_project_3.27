dcb6baa7066a9ef399e658e6fa9e31cef621fd00
ndemeyvan <ndemeyvan@gmail.com>
2025-04-19 16:25:38 +0100

    work on setup project

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,24 +30,6 @@ android {
         versionName = flutter.versionName
     }
 
-       flavorDimensions "default"
-    productFlavors { 
-        production {
-            dimension "default"
-            applicationIdSuffix ""
-            manifestPlaceholders = [appName: "Maliya"]
-        }
-        staging {
-            dimension "default"
-            applicationIdSuffix ".stg"
-            manifestPlaceholders = [appName: "[STG] Maliya"]
-        }
-        development {
-            dimension "default"
-            applicationIdSuffix ".dev"
-            manifestPlaceholders = [appName: "[DEV] Maliya"]
-        }
-    }
 
     buildTypes {
         release {
